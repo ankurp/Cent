9217e964ad48c6bb5eba893a546d02f714292163
José Iván Peñuelas Ontiveros <joseivanpo01@gmail.com>
2023-01-12 13:23:52 -0700

    fix

--- Sources/Regex.swift
+++ Sources/Regex.swift
@@ -40,7 +40,7 @@ public class Regex {
 
     public class func escapeStr(str: String) -> String {
         let matches = RegexPatternRegex.matches(testStr: str)
-        var charArr = [Character](str.characters)
+        var charArr = [Character](str)
         var strBuilder = [Character]()
         var i = 0
         for match in matches {
